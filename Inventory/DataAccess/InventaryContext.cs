c17c0949bd3e0e9df3674a6a9942c1a2632f21ed
Maxruizdz <maxruizdz@hotmail.com>
2023-09-08 18:47:00 -0300

    Creando el datacontext

---
+++ Inventory/DataAccess/InventaryContext.cs
@@ -0,0 +1,54 @@
+﻿using Inventory.Entity;
+using Inventory.Entity.obj;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Inventory.DataAccess
+{
+    public class InventaryContext: DbContext
+    { 
+        public DbSet<ProductEntity> productEntities { get; set; }
+        public DbSet<CategoriaEntity> categoriaEntities { get; set; }
+        public DbSet<InOutEntity> inventaryEntities { get; set;}
+
+        public DbSet<StorageEntity> storageEntities { get; set; }
+
+        public DbSet<WherehouseEntity> wherehouseEntities { get; set; }
+
+
+        public InventaryContext(DbContextOptions<InventaryContext> options) : base(options) {
+        
+        
+        
+        
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder) {
+
+
+
+
+
+
+
+            modelbuilder.Entity<ProductEntity>(producto =>
+            {
+               
+
+
+            });
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+
+    }
+}
